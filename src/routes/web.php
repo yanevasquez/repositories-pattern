94badcfcfc9fa0179b89f63410c3075cca379b50
Yane <yanelisset4@gmail.com>
2021-04-01 19:37:04 -0300

    feat(auth): add authentication module configuration

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,16 +1,18 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+$this->any('admin/products/search', 'Admin\ProductController@search')->name('products.search');
+$this->resource('admin/products', 'Admin\ProductController');
+
+Route::get('admin', function () {
+})->name('admin');
+
+//Route::any('admin/categories/search', 'Admin\CategoryController@search')->name('categories.search');
+//Route::resource('admin/categories', 'Admin\CategoryController');
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
