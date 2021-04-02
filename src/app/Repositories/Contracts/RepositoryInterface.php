846c404218b02cbfc331bd85ef3ec01a18fe1ef0
Yane <yanelisset4@gmail.com>
2021-04-01 21:20:53 -0300

    refactor(repository): start creating repository interface

---
+++ src/app/Repositories/Contracts/RepositoryInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Repositories\Contracts;
+
+
+interface RepositoryInterface
+{
+    public function getAll();
+    public function findById($id);
+    public function findWhere($column, $valor);
+    public function paginate($totalPage = 10);
+    public function store(array $data);
+    public function update($id, array $data);
+    public function delete($id);
+}
